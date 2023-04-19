7da2d056a7b8752822470e8ab2cab5b5767371ed
Dona Siby <dona.c.siby@gmail.com>
2023-04-19 19:47:48 +0530

    minor editing

--- Screens/Products/ProductContainer.js
+++ Screens/Products/ProductContainer.js
@@ -1,11 +1,13 @@
 import React, { useState,useEffect } from 'react'
 import { View,Text,StyleSheet,ActivityIndicator,FlatList} from 'react-native'
+
+
 const data=require('../../assets/data/products.json')
+
 const ProductContainer = () => {
     const[products,setProducts]= useState([]);
     useEffect(() => {
         setProducts(data);
-
         return()=>{
             setProducts([])
         }
