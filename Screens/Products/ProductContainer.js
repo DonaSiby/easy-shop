c703a92ced0067fd3dcaef310065011558ada626
Dona Siby <dona.c.siby@gmail.com>
2023-04-25 19:03:28 +0530

    recent changes

--- Screens/Products/ProductContainer.js
+++ Screens/Products/ProductContainer.js
@@ -2,6 +2,8 @@ import React, { useState,useEffect } from 'react'
 import { View,Text,StyleSheet,ActivityIndicator,FlatList} from 'react-native'
 
 
+import ProductList from './ProductList'
+
 const data=require('../../assets/data/products.json')
 
 const ProductContainer = () => {
@@ -18,9 +20,13 @@ const ProductContainer = () => {
             <Text>Product Container</Text>
             <View style={{marginTop:100}}>
             <FlatList
-                horizontal
+                numColumns={2}
+                //horizontal
                 data={products}
-                renderItem={(item)=><Text>{item.brand}</Text>}
+                renderItem={(item)=>
+                <ProductList 
+                key={item.id}
+                item={item}/>}
                 keyExtractor={item=>item.name}
             />
             </View>
